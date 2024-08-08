496ba952e9d5476831d3ff3b1a5ce4e186db8d5f
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-08 17:20:07 +0500

    db configuring

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        imports: [],
+        inject: [],
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: String(process.env.DATABASE_PASSWORD),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
