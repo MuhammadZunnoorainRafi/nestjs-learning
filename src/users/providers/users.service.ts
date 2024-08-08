fce53fb1ab660b236cbaae3c6be0da3a098a8794
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-08 16:28:29 +0500

    compodoc

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,6 +1,9 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUserParamsDto } from '../dtos/get-user-params.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
+/**
+ * Class to connect to users table and perform business operations
+ */
 
 @Injectable()
 export class UsersService {
@@ -8,6 +11,9 @@ export class UsersService {
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+  /**
+   * The method to get all the users from the database
+   */
 
   public findAll(
     getUserParamsDto: GetUserParamsDto,
@@ -20,6 +26,10 @@ export class UsersService {
       { name: 'Tom', email: 'tom@gmail.com' },
     ];
   }
+
+  /**
+   * Find a single user using the ID of the user
+   */
 
   public findOneById(id: string) {
     return { id, name: 'Zain', email: 'zain@gmail.com' };
