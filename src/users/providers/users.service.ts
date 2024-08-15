aeee494aaacdc0377068b8b4ed1917acb7cbeb2a
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-15 20:14:35 +0500

    Exception handling

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,4 +1,13 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  forwardRef,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { GetUserParamsDto } from '../dtos/get-user-params.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { Repository } from 'typeorm';
@@ -21,12 +30,27 @@ export class UsersService {
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
-    const userExists = await this.userRepository.findOne({
-      where: { email: createUserDto.email },
-    });
-    let newUser = this.userRepository.create(createUserDto);
-    newUser = await this.userRepository.save(newUser);
-    return newUser;
+    try {
+      const userExists = await this.userRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+
+      if (userExists) {
+        return new BadRequestException(
+          'The user already exists, please check your email.',
+        ).getResponse();
+      }
+
+      const newUser = this.userRepository.create(createUserDto);
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      return new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to the the datbase',
+        },
+      ).getResponse();
+    }
   }
 
   /**
@@ -37,20 +61,38 @@ export class UsersService {
     page: number,
     limit: number,
   ) {
-    const environment = this.configService.get('TEST');
-    console.log(environment);
-
-    const user = this.authService.isAuth();
-    return [
-      { name: 'John', email: 'johndoe@gmail.com' },
-      { name: 'Tom', email: 'tom@gmail.com' },
-    ];
+    throw new HttpException(
+      {
+        status: HttpStatus.MOVED_PERMANENTLY,
+        error: 'The API endpoint does not exist',
+        fileName: 'users.service.ts',
+        lineNumber: 88,
+      },
+      HttpStatus.MOVED_PERMANENTLY,
+      {
+        cause: new Error(),
+        description: 'Occured because the API endpoint was permanently moved',
+      },
+    );
   }
 
   /**
    * Find a single user using the ID of the user
    */
   public async findOneById(id: string) {
+    // try {
     return await this.userRepository.findOneBy({ id });
+    // if (!user) {
+    //   return new NotFoundException('User not found').getResponse();
+    // }
+    // return user;
+    // } catch (error) {
+    //   return new RequestTimeoutException(
+    //     'Unable to process your request at the moment please try later',
+    //     {
+    //       description: 'Error connecting to the the datbase',
+    //     },
+    //   ).getResponse();
+    // }
   }
 }
