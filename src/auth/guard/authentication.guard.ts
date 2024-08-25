d0048d3926b627e31ce00b905c7cf21e8a0a9ef6
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-25 18:41:10 +0500

    Authtentication Guard implemented

--- src/auth/guard/authentication.guard.ts
+++ src/auth/guard/authentication.guard.ts
@@ -1,8 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-import { AccessTokenGuard } from './access-token.guard';
+import { AUTH_TYPE_KEY } from '../constants/auth.constants';
 import { AuthType } from '../enums/auth-type.enum';
+import { AccessTokenGuard } from './access-token.guard';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -20,10 +25,32 @@ export class AuthenticationGuard implements CanActivate {
     private readonly accessTokenGuard: AccessTokenGuard,
   ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(this.authTypeGuardMap);
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // authTypes from reflector
+    const authTypes =
+      this.reflector.getAllAndOverride(AUTH_TYPE_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? AuthenticationGuard.defaultAuthType;
+    // console.log({ AuthType, authTypes }); // [0]
+    // array of guards
+    const guards = authTypes
+      .map((type: number) => this.authTypeGuardMap[type])
+      .flat();
+    // console.log(guards);
+    // Default error
+    const error = new UnauthorizedException();
+    // Loop guards canActivate
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch((err) => {
+        error: err;
+      });
+      if (canActivate) {
+        return true;
+      }
+    }
+    throw error;
   }
 }
